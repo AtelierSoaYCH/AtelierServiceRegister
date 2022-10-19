664c73d55f80f481e6ad1c41e8cebb85e756c615
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-19 13:19:42 +0100

    first commit

---
+++ src/main/java/com/iset/serviceregister/ServiceRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.iset.serviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iset/serviceregister/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iset.serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
